b0d4ac3eea19dd0d7df56d5f35527eb359a632e6
naor.harel@hp.com <naor.harel@hp.com>
2022-04-05 19:42:45 +0300

    adding boostrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const element = <h1>Hello world ()</h1>;
+console.log(element);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
